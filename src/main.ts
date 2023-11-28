837e2c165022526ccd1ffa291924ceaaaa29e292
thaiquangbao <97949012+thaiquangbao@users.noreply.github.com>
2023-11-28 23:14:11 +0700

    upload

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express/interfaces';
+import * as hbs from 'hbs';
+import * as hbsUtils from 'hbs-utils';
 
 async function bootstrap() {
   //const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
+  hbsUtils(hbs).registerWatchedPartials(join(__dirname, '..', 'views/layouts'));
   app.setViewEngine('hbs');
   await app.listen(3000);
 }
