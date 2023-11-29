db507300f9c4a6238fbd29a1f2a2fea4ac0bfb87
thaiquangbao <97949012+thaiquangbao@users.noreply.github.com>
2023-11-30 06:01:07 +0700

    upload

--- dist/app.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AppService } from "./app.service";
-import { Response } from "express";
-import { FoodService } from "./food/food.service";
-export declare class AppController {
-    private readonly foodService;
-    private readonly appService;
-    constructor(foodService: FoodService, appService: AppService);
-    getAllFood(res: Response): Promise<void>;
-}
