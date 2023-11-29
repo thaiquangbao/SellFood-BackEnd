a7f5802b287ebbc43e34d6f16073e87ab279ca64
thaiquangbao <97949012+thaiquangbao@users.noreply.github.com>
2023-11-30 06:00:50 +0700

    upload

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, "..", "public"));
   app.setBaseViewsDir(join(__dirname, "..", "views"));
-  hbs.registerPartials(join(__dirname, "..", "views/layouts"));
+  hbs.registerPartial(
+    "layout",
+    join(__dirname, "..", "views/layouts/layout.hbs"),
+  );
   hbsUtils(hbs).registerWatchedPartials(
     join(__dirname, "..", "views/partials"),
   );
