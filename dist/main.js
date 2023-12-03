a488ffaf04b9784ad40231680bea6bc79566ff1a
thaiquangbao <97949012+thaiquangbao@users.noreply.github.com>
2023-12-03 16:08:59 +0700

    upload

--- dist/main.js
+++ dist/main.js
@@ -3,19 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const path_1 = require("path");
-const handlebars = require("handlebars");
-const fs = require("fs");
+const hbs = require("hbs");
+const hbsUtils = require("hbs-utils");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useStaticAssets((0, path_1.join)(__dirname, "..", "public"));
     app.setBaseViewsDir((0, path_1.join)(__dirname, "..", "views"));
-    const layoutsDir = (0, path_1.join)(__dirname, "..", "views/layouts");
-    const layouts = fs.readdirSync(layoutsDir);
-    layouts.forEach((layout) => {
-        const layoutPath = (0, path_1.join)(layoutsDir, layout);
-        const layoutContent = fs.readFileSync(layoutPath, "utf-8");
-        handlebars.registerPartial(layout, layoutContent);
-    });
+    hbs.registerPartials((0, path_1.join)(__dirname, "..", "views/layouts"));
+    hbsUtils(hbs).registerWatchedPartials((0, path_1.join)(__dirname, "..", "views/partials"));
     app.setViewEngine("hbs");
     app.set("view options", {
         layout: "layouts/main",
--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,17 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { join } from "path";
 import { NestExpressApplication } from "@nestjs/platform-express/interfaces";
-//import * as hbs from "hbs";
-//import * as hbsUtils from "hbs-utils";
-import * as handlebars from "handlebars";
-import * as fs from "fs";
+import * as hbs from "hbs";
+import * as hbsUtils from "hbs-utils";
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, "..", "public"));
   app.setBaseViewsDir(join(__dirname, "..", "views"));
-  const layoutsDir = join(__dirname, "..", "views/layouts");
-  const layouts = fs.readdirSync(layoutsDir);
-
-  layouts.forEach((layout) => {
-    const layoutPath = join(layoutsDir, layout);
-    const layoutContent = fs.readFileSync(layoutPath, "utf-8");
-    handlebars.registerPartial(layout, layoutContent);
-  });
-  //hbsUtils(hbs).registerPartials(join(__dirname, "..", "views/layouts"));
-  // hbsUtils(handlebars).registerWatchedPartials(
-  //   join(__dirname, "..", "views/partials"),
-  // );
+  hbs.registerPartials(join(__dirname, "..", "views/layouts"));
+  hbsUtils(hbs).registerWatchedPartials(
+    join(__dirname, "..", "views/partials"),
+  );
   app.setViewEngine("hbs");
   app.set("view options", {
     layout: "layouts/main",
